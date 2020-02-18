11c98506a208ce73bedaca6d1e56a1e864bed080
부섭 조 <egm7126@naver.com>
2020-02-19 01:14:39 +0900

    exep2의 이전

---
+++ exep3/exep3/소스.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "hello world, this is test of plug-in of visual studio" << endl;
+
+	typedef int newint;
+
+	newint a = 1;
+	newint b = 2;
+
+	cout << "a+b= " << a + b << endl;
+}
